d7b7b4fb375e38cdc790c177d2348bac400a4186
cherryzhong@efun.com <cherryzhong@efun.com>
2017-08-01 22:20:25 +0800

    我的第一个SpringBoot案例，太神奇了！

---
+++ src/main/java/action/SpringBootExample.java
@@ -0,0 +1,24 @@
+package action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 我的第一个SpringBoot案例，太神奇了！
+ * @author zhongzhenhao
+ * 用main方法来跑动tomcat，而且全部东西都是已经内置好了，太方便了！！！
+ */
+@RestController
+@EnableAutoConfiguration
+public class SpringBootExample {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {		
+		SpringApplication.run(SpringBootExample.class, args);		
+	}
+
+}
